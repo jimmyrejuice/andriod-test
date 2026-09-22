4c35983d6d4f11a70d0a8e144317deb4ace64427
jimmyrejuice <121598590+jimmyrejuice@users.noreply.github.com>
2026-09-22 18:02:55 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,13 +8,14 @@ android {
     namespace = "com.example.quickswitch"
     compileSdk = 35
 
-    defaultConfig {
+defaultConfig {
     applicationId = "com.example.quickswitch"
     minSdk = 29
     targetSdk = 35
-    versionCode = 3
-    versionName = "1.2"
+    versionCode = 4
+    versionName = "1.3"
 }
+
     buildTypes {
         release {
             isMinifyEnabled = false
