fcc91f1326b58c35083229a0ab883771d435637b
jimmyrejuice <121598590+jimmyrejuice@users.noreply.github.com>
2026-09-23 10:08:42 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.quickswitch"
         minSdk = 29
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.3.1"
+        versionCode = 6
+        versionName = "1.3.2"   
     }
 
     buildTypes {
