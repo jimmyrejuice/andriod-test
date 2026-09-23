cc8439255e658f700ad6e45827839fbd37f69b6f
jimmyrejuice <121598590+jimmyrejuice@users.noreply.github.com>
2026-09-23 10:14:01 +0800

    Update SleepScreen.kt

--- app/src/main/java/com/example/quickswitch/SleepScreen.kt
+++ app/src/main/java/com/example/quickswitch/SleepScreen.kt
@@ -22,12 +22,10 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.DarkMode
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowLeft
 import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material.icons.filled.KeyboardArrowUp
-import androidx.compose.material.icons.filled.LightMode
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
